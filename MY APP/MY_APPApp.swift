9ea8534637da5692063623d004610519be197910
Raghad <ragadtalhi@gmail.com>
2024-09-29 09:25:01 +0300

    Initial Commit

---
+++ MY APP/MY_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_APPApp.swift
+//  MY APP
+//
+//  Created by Raghad Altalhi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MY_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
